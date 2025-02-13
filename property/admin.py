66a0fb490d88037527d76291c54ec9fe78d8adb8
VASILIYKAS <yoyoynv@gmail.com>
2025-02-13 16:50:32 +0500

    Added a list of owners on the flats page

--- property/admin.py
+++ property/admin.py
@@ -3,6 +3,10 @@ from django.contrib import admin
 from .models import Flat, Complaint, Owner
 
 
+class FlatsInline(admin.TabularInline):
+    model = Flat.owners.through
+    raw_id_fields = ('owner',)
+
 class FlatAdmin(admin.ModelAdmin):
     search_fields = ('owner', 'town', 'address',)
     readonly_fields = ['created_at',]
@@ -14,7 +18,7 @@ class FlatAdmin(admin.ModelAdmin):
     list_editable = ('new_building',)
     list_filter = ('new_building',)
     raw_id_fields = ('liked_by',)
-
+    inlines = [FlatsInline]
 
 admin.site.register(Flat, FlatAdmin)
 
